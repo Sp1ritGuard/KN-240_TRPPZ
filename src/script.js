7040295d101faeeebe806d1dbf3df5ed85ea76c1
Nazar Rak <nazar.snow2024@gmail.com>
2025-02-21 13:22:20 +0200

    commit

--- src/script.js
+++ src/script.js
@@ -1,23 +1,31 @@
+document.getElementById("change-title").addEventListener("click", function () {
+    document.getElementById("main-title").textContent = "Заголовок змінено!";
+});
+
+
 function greetUser(name) {
     console.log(`Привіт, ${name}!`);
 }
+greetUser("Данило");
+
 
 function checkAge() {
-    let age;
-    do {
-        age = prompt("Введіть ваш вік:");
-        if (isNaN(age) || age.trim() === "") {
-            alert("Будь ласка, введіть число!");
-        } else {
-            age = Number(age);
-            if (age < 18) {
-                alert("Доступ заборонено");
-            } else {
-                alert("Доступ дозволено");
-            }
-        }
-    } while (isNaN(age) || age.trim() === "");
+    let age = prompt("Введіть свій вік:");
+
+    while (isNaN(age) || age === "" || age === null) {
+        age = prompt("Будь ласка, введіть число!");
+    }
+
+    age = Number(age);
+
+    if (age < 18) {
+        alert("Доступ заборонено");
+    } else {
+        alert("Доступ дозволено");
+    }
 }
+checkAge();
+
 
 function getRandomRGB() {
     let r = Math.floor(Math.random() * 256);
@@ -26,9 +34,7 @@ function getRandomRGB() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-document.body.addEventListener("click", function() {
+
+document.body.addEventListener("click", function () {
     document.body.style.backgroundColor = getRandomRGB();
 });
-
-
-checkAge();
